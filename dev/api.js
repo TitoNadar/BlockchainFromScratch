6dc28e948f364e497bc0433105909fb7bc375f17
TitoNadar <titonadar06@gmail.com>
2018-09-22 12:03:36 +0530

    get blockhain done

--- dev/api.js
+++ dev/api.js
@@ -1,4 +1,13 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get entire blockchain
+app.get('/blockchain', function (req, res) {
+    res.send(bitcoin);
+  });
