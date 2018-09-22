6ab44bccead9bcb5115e8fe36fc57d68037351ab
TitoNadar <titonadar06@gmail.com>
2018-09-22 11:01:15 +0530

    block  creation  function added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,3 +3,19 @@ function Blockchain() {
 	this.chain = [];
 this.pendingTransactions = [];
 }
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+	const newBlock = {
+		index: this.chain.length + 1,
+		timestamp: Date.now(),
+		transactions: this.pendingTransactions,
+		nonce: nonce,
+		hash: hash,
+		previousBlockHash: previousBlockHash
+	};
+
+	this.pendingTransactions = [];
+	this.chain.push(newBlock);
+
+	return newBlock;
+};
