f33f795a5bb36ea9a005574432bd7391dea22c52
TitoNadar <titonadar06@gmail.com>
2018-09-22 11:18:54 +0530

    proof of work function added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -41,3 +41,14 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
 	const hash = sha256(dataAsString);
 	return hash;
 };
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+	let nonce = 0;
+	let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	while (hash.substring(0, 4) !== '0000') {
+		nonce++;
+		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	}
+
+	return nonce;
+};
