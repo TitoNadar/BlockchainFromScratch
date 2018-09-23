5ebfeb2e855aa279f7e45d6e14b91bcb025d9717
TitoNadar <titonadar06@gmail.com>
2018-09-23 15:28:37 +0530

    1st stage done

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,6 @@
 const sha256=require('sha256');
 const currentNodeUrl = process.argv[3];
+const uuid = require('uuid/v1');
 
 function Blockchain() {
 	this.chain = [];
@@ -41,6 +42,11 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
 	};
 
 	return newTransaction;
+};
+
+Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj) {
+	this.pendingTransactions.push(transactionObj);
+	return this.getLastBlock()['index'] + 1;
 };
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
